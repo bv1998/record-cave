90775b1671597ce02493fd73c3bb8702da03e0b7
Bryan Vernon <bryan.vernon9@icloud.com>
2024-06-06 07:29:50 -0400

    Updated Config to use ENV Variables

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+        define: {
+            "process.env.CLIENT_ID": JSON.stringify(env.CLIENT_ID),
+            "process.env.CLIENT_SECRET": env.CLIENT_SECRET,
+        },
+    };
+});
